669d077f057615bca978eabc7e2d8d345b3fb295
nmr <nmrhard@gmail.com>
2021-05-30 21:58:47 +0300

    Using materail icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="project">
-    <div class="actions" @click="toggleDetails">
-      <h3>{{ project.title }}</h3>
+    <div class="actions">
+      <h3 @click="toggleDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div v-if="isShowDetails" class="details">
       <p>{{ project.details }}</p>
@@ -37,5 +42,19 @@ export default {
   }
   h3 {
     cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
   }
 </style>
