fe58300eb457ccdf14497f6e07c227dc6fb41ff5
nmr <nmrhard@gmail.com>
2021-06-01 23:33:08 +0300

    Creating a filter nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current"/>
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filterProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -10,15 +11,17 @@
 
 <script>
 import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name: 'Home',
   components: {
-    SingleProject
+    SingleProject, FilterNav
   },
   data() {
     return {
       projects: [],
+      current: 'all'
     }
   },
   methods: {
@@ -30,11 +33,23 @@ export default {
       p.complete = !p.complete;
     }
   },
+  computed: {
+    filterProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+
+      return this.projects;
+    }
+  },
   mounted() {
     fetch(' http://localhost:3000/projects')
       .then(res => res.json())
       .then(data => this.projects = data)
       .catch(err => console.log(err.message))
-  }
+  },
 }
 </script>
